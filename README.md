23f78a6e5cf3410c32ad3b37ad80b0bdfe2cf27f
Max Leske <maxleske@gmail.com>
2019-03-12 22:47:06 +0100

    initial

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # git-xd-merge
 Git merge driver for Adobe XD files
+
+## Requirements
+- https://github.com/jonatanpedersen/git-json-merge
+- python3
+- git
+
+## Installation
+- put the python executable into your home directory
+- install https://github.com/jonatanpedersen/git-json-merge (`npm install --global git-json-merge`)
+- add the following to your `.gitconfig` file:
+                [merge "json"]
+                    name = custom merge driver for json files
+                    driver = git-json-merge %A %O %B
+                [merge "agc"]
+                    name = custom merge driver for json files
+                    driver = git-json-merge %A %O %B
+                [merge "xd"]
+                    name = custom merge driver for XD files
+                    driver = python3 ~/xd-merge.py %A %O %B
+- add the following to your `.gitattributes` file:
+                *.json merge=json
+                *.agc merge=json
+                *.xd merge=xd
---
+++ xd-merge.py
@@ -0,0 +1,45 @@
+#/usr/bin/env python3
+
+import sys
+from zipfile import ZipFile, BadZipFile
+import tempfile
+import subprocess
+import shutil
+import os.path
+
+def run(ancestor, current, other):
+	path = os.getcwd()
+	import pdb; pdb.set_trace()
+	with tempfile.TemporaryDirectory() as directory:
+		subprocess.run(["git", "init"], cwd=directory)
+
+		subprocess.run(["git", "checkout", "-b", "ancestor"], cwd=directory)
+		ZipFile(os.path.join(path, ancestor)).extractall(directory)
+		subprocess.run(["git", "add", "-A"], cwd=directory)
+		subprocess.run(["git", "commit", "-am", "'foo'"], cwd=directory)
+
+		subprocess.run(["git", "checkout", "-b", "current"], cwd=directory)
+		try:
+			ZipFile(os.path.join(path, current)).extractall(directory)
+			subprocess.run(["git", "add", "-A"], cwd=directory)
+			subprocess.run(["git", "commit", "-am", "'foo'"], cwd=directory)
+		except BadZipFile:
+			pass
+
+		subprocess.run(["git", "checkout", "ancestor"], cwd=directory)
+		subprocess.run(["git", "checkout", "-b", "other"], cwd=directory)
+		ZipFile(os.path.join(path, other)).extractall(directory)
+		subprocess.run(["git", "commit", "-am", "'foo'"], cwd=directory)
+		
+		subprocess.run(["git", "checkout", "current"], cwd=directory)
+		completed = subprocess.run(["git", "merge", "other", "-m", "'foo'"], capture_output=True, cwd=directory)
+		if completed.returncode != 0:
+			sys.exit(-1)
+
+		subprocess.run(["rm", "-rf", ".git"], cwd=directory)
+		subprocess.run(["zip", "-r", "stuff", "."], cwd=directory)
+		shutil.copy(os.path.join(directory, 'stuff.zip'), os.path.join(path, current))
+
+		sys.exit(0)
+
+run(*sys.argv[1:])
